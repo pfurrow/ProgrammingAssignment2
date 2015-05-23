1a13da177524284c39d3f2e50b0e577fbf874d0a
pfurrow <pat.furrow@sabre.com>
2015-05-22 23:01:11 -0600

    Final commented code

--- cachematrix.R
+++ cachematrix.R
@@ -5,48 +5,62 @@
 ## without recomputing it.  Here is a simple example of the use of this
 ## pair of functions:
 
-## Initialize the caching functions for a 4 x 4 numeric matrix identified as 
-## my_m which in this simple case we will define with 16 random values:
+## Initialize the list of caching functions assigned to my_m for a 4 x 4 numeric  
+## matrix which in this simple case we will generate with 16 random values:
 ##
 ##      my_m <- makeCacheMatrix(matrix(rnorm(16), 4, 4))
 
-## Now whenever the inverse of the matrix assigned to my_m is needed, 
-## instead of invoking solve() directly, use the cacheSolve function
-## to  calculate the inverse only once and then efficiently 
-## retrieve that inverse matrix from cache on all subsequent calls:
-##
-##      my_inv <- cacheSolve(my_m)
-
 makeCacheMatrix <- function(x = matrix()) {
-        ## 'x' is a square numeric or complex matrix for which 
-        ## the inverse will be needed repeatedly.
+        ## 'x' is a square numeric or complex matrix for which the inverse will
+        ## be needed repeatedly.  It is assumed to be invertible.
+        ##
+        ## This function generates a list of four caching functions which
+        ## 1) store in and 2) retrieve from cache the matrix passed in the
+        ## function call and which 3) store in and 4) retrieve from cache the 
+        ## inverse of the passed in matrix.  These functions are called from the
+        ## cacheSolve function and therefore make use of a unique environment
+        ## to maintain the matrix, it's inverse and the function definitions.
         
-        i <- NULL
-        set <- function(y) {
-                x <<- y
-                i <<- NULL
+        i <- NULL                            ## 1) save the passed matrix in the
+        set <- function(y) {                 ## unique environment and initialize  
+                x <<- y                      ## the inverse variable 
+                i <<- NULL                  
         }
-        get <- function() x
-        setinv <- function(inv) i <<- inv
-        getinv <- function() i
-        list(set = set, get = get, 
-             setinv = setinv,
+
+        get <- function() x                  ## 2) return the original matrix
+
+        setinv <- function(inv) i <<- inv    ## 3) initialize i with the passed
+                                             ## inverse matrix
+
+        getinv <- function() i               ## 4) return the stored inverse matrix
+
+        list(set = set, get = get,           ## generate the list of 4 functions
+             setinv = setinv,                ## in the unique environment
              getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Now whenever the inverse of the matrix assigned to my_m is needed, 
+## instead of invoking solve() directly, use the cacheSolve function
+## to  calculate the inverse only once, save it in cache and then efficiently 
+## retrieve that inverse matrix from cache on all subsequent calls:
+##
+##      my_inv <- cacheSolve(my_m)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## 'x' is the variable to which the makeCacheMatrix function was 
+        ## previously assigned.  It identifies a set of functions and an
+        ## environment unique to the matrix that was specificed in the 
+        ## preceeding makeCacheMatrix function call.
         
-        i <- x$getinv()
-        if(!is.null(i))  {
-                message("getting cached data")
-                return(i)
+        i <- x$getinv()                          ## retrieve the matrix inverse
+                                                 ## from cache
+        if(!is.null(i))  {                       ## if it is not null
+                message("getting cached data")   ## indicate the cached inverse
+                return(i)                        ## is being returned
         }
-        data <- x$get()
-        i <- solve(data, ...)
-        x$setinv(i)
-        i
+        data <- x$get()                          ## otherwise, get the matrix,
+        i <- solve(data, ...)                    ## generate the inverse,  
+        x$setinv(i)                              ## store it in cache,
+        i                                        ## and return it
 }
